bdbf0fc502a7fa23e379b10b0924bcaca10ae018
Johnatan Botero <johnatna@jbotero.com>
2017-09-11 09:36:46 -0500

    Storage

---
+++ server/Chat/index.js
@@ -0,0 +1,20 @@
+var express = require('express')
+var Router = express.Router()
+
+Router.get ('/users', function(req, res){
+
+})
+
+Router.get ('/admin', function(req, res){
+
+})
+
+Router.get ('/dashboard', function(req, res){
+
+})
+
+Router.get ('/messages', function(req, res){
+
+})
+
+module.exports = Router
--- server/index.js
+++ server/index.js
@@ -1,15 +1,19 @@
 var bodyParser = require('body-parser'),
-    http        = require('http'),
-    express     = require('express')
+    http = require('http'),
+    express = require('express'),
+    chat = require('./chat')
 
-var port        = port  = process.env.PORT || 3000,
-    app         = express(),
-    Server      = http.createServer(app)
+var port = port = process.env.PORT || 3000,
+    app = express(),
+    Server = http.createServer(app)
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use('/chat', chat)
+
 app.use(express.static('public'))
 
-Server.listen(port, fuction(){
+Server.listen(port, function() {
   console.log("server is running on port: " + port)
 })
